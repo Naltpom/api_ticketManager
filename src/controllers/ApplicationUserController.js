240decc74b6b00c9f8423b45d7f862853ed0de8d
Naltpom <nathan.provost78990@gmail.com>
2021-03-15 21:56:03 +0100

    TM-008
    ApplicationUser crud + routes
    can't have 1 users have 2 documents of the same application

--- src/controllers/ApplicationUserController.js
+++ src/controllers/ApplicationUserController.js
@@ -12,44 +12,137 @@ exports.createApplicationUser = (req, res, next) => {
     const user = User.findOne({_id: companyUserObject.user_id})
         .then(user => {
             if (!user) {
-                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+                return res.status(401).json({ error1: 'Utilisateur non trouvé !' });
             } 
             return user;
         })
-        .catch(err => res.status(400).json({ message: "id invalide", error: err }))
+        .catch(error => res.status(400).json({ message: "id invalide", error: error }))
 
     const application = Application.findOne({_id: companyUserObject.application_id})
         .then(application => {
             if (!application) {
-                return res.status(401).json({ error: 'Application non trouvé !' });
+                return res.status(401).json({ error2: 'Application non trouvé !' });
             } 
             return application
         })
-        .catch(err => res.status(400).json({ err }))
+        .catch(error => res.status(400).json({ error }))
 
 
     Promise.all([user, application])
         .then(data => {
-            console.log(data)
             dataUser = data[0];
             dataApplication = data[1];
-            if (dataUser instanceof ServerResponse || dataApplication instanceof ServerResponse) {
-                console.log('totototoototototototototoototototo');;
+            if (dataUser instanceof User && dataApplication instanceof Application) {
+                ApplicationUser.findOne({user_id: dataUser._id,application_id: dataApplication._id})
+                    .then(app => {
+                        console.log(app);
+                        if (app === [] || app === null) {
+                            
+                            const company = new ApplicationUser({
+                                user_id: dataUser._id,
+                                application_id: dataApplication._id,
+                                roles: companyUserObject.roles,
+                                created_by: null,
+                                update_by: null,
+                                created_at: Date.now(),
+                                update_at: Date.now(),
+                            })
+                
+                            company.save()
+                                .then((applicationUser) => res.status(201).json({ message: 'ApplicationUser crée !', applicationUser: applicationUser}))
+                                .catch(error => res.status(400).json({ error }));
+
+                        } else {
+                            /**
+                             * this sould edit existing one or not ?
+                             * in the mean time we return error cause it exist
+                             */
+                            return res.status(401).json({ error2: 'ApplicationUser deja existant' });
+                        }
+
+                    })
+            } else {
+                return;
             }
             
-            const company = new ApplicationUser({
-                user_id: dataUser._id,
-                application_id: dataApplication._id,
-                roles: companyUserObject.roles,
-                created_by: null,
-                update_by: null,
-                created_at: Date.now(),
-                update_at: Date.now(),
-            })
 
-            company.save()
-                .then((applicationUser) => res.status(201).json({ message: 'ApplicationUser crée !', applicationUser: applicationUser}))
-                .catch(error => res.status(400).json({ error }));
         })
     .catch(error => res.status(400).json({ error }));
+};
+
+exports.modifyApplicationUser = (req, res, next) => {
+    const companyUserObject = req.body.applicationUser;
+
+    const user = User.findOne({_id: companyUserObject.user_id})
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({ error1: 'Utilisateur non trouvé !' });
+            } 
+            return user;
+        })
+        .catch(error => res.status(400).json({ message: "id invalide", error: error }))
+
+    const application = Application.findOne({_id: companyUserObject.application_id})
+        .then(application => {
+            if (!application) {
+                return res.status(401).json({ error2: 'Application non trouvé !' });
+            } 
+            return application
+        })
+        .catch(error => res.status(400).json({ error }))
+
+
+    Promise.all([user, application])
+        .then(data => {
+            dataUser = data[0];
+            dataApplication = data[1];
+            if (dataUser instanceof User && dataApplication instanceof Application) {
+                ApplicationUser.findOne({user_id: dataUser._id,application_id: dataApplication._id})
+                    .then(app => {
+                        if (app !== []) {
+                            ApplicationUser.updateOne({}, {
+                                user_id: dataUser._id,
+                                application_id: dataApplication._id,
+                                roles: companyUserObject.roles,
+                                created_by: null,
+                                update_by: null,
+                                created_at: app.created_at,
+                                update_at: Date.now(),
+                            })
+                                .then((applicationUser) => res.status(201).json({ message: 'ApplicationUser modifié !', applicationUser: applicationUser}))
+                                .catch(error => res.status(400).json({ error }));
+                        } else {
+                            return res.status(401).json({ error2: 'ApplicationUser non trouvé !' });
+                        }
+                    })
+                    .catch()
+            }
+        })
+        .catch(error => res.status(400).json({ error }))
+};
+
+exports.deleteApplicationUser = (req, res, next) => {
+    ApplicationUser.findOne({ _id: req.params.id })
+        .then(applicationUser => {
+            if (applicationUser !== null) {
+                ApplicationUser.deleteOne({ _id: req.params.id })
+                    .then(() => res.status(200).json({message : 'ApplicationUser supprimé'}))
+                    .catch(error => res.status(400).json({ error }));
+            } else {
+                return res.status(404).json({ message: 'ApplicationUser non trouvé' })
+            }
+        })
+        .catch(error => res.status(500).json({ error }));
+};
+
+exports.getOneApplicationUser = (req, res, next) => {
+    ApplicationUser.findOne({ _id: req.params.id })
+        .then(applicationUser => res.status(200).json(applicationUser))
+        .catch(error => res.status(404).json({ error }));
+};
+
+exports.getAllApplicationUsers = (req, res, next) => {
+    ApplicationUser.find()
+        .then(applicationUser => res.status(200).json(applicationUser))
+        .catch(error => res.status(400).json({ error }));
 };
--- src/routes/ApplicationUserRoute.js
+++ src/routes/ApplicationUserRoute.js
@@ -3,11 +3,11 @@ const express = require('express');
 const router = express.Router();
 const applicationUser = require('../controllers/ApplicationUserController');
 
-// router.get('/', applicationUser.getAllApplicationUsers);
+router.get('/', applicationUser.getAllApplicationUsers);
 router.post('/', applicationUser.createApplicationUser);
-// router.get('/:id', applicationUser.getOneApplicationUser);
-// router.put('/:id', applicationUser.modifyApplicationUser);
-// router.delete('/:id', applicationUser.deleteApplicationUser);
+router.get('/:id', applicationUser.getOneApplicationUser);
+router.put('/:id', applicationUser.modifyApplicationUser);
+router.delete('/:id', applicationUser.deleteApplicationUser);
 
 
 module.exports = router;
